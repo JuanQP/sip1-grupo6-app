989bc7f8142b3ba37fd8b36f41cbd54bcefe05b4
JuanQuinteros <juan.quinteros.dev@gmail.com>
2022-05-15 16:25:42 -0300

    Refactors para poner el código prolijo 💖

--- utils/utils.js
+++ utils/utils.js
@@ -1,12 +1,35 @@
+/**
+ * Una lista de números del 0 al "size"
+ * @param {*} size
+ * @returns number[]
+ */
 export function range(size) {
   return [...Array(size).keys()];
 }
 
-export function momentToMarkedDate(moment) {
+/**
+ * Devuelve un rango de fechas
+ * @param {moment.Moment} start_date
+ * @param {moment.Moment} end_date
+ * @returns moment.Moment[]
+ */
+export function momentRange(start_date, end_date) {
+  const daysCount = end_date.diff(start_date, 'days');
+  return [
+    ...range(daysCount).map(i => start_date.clone().add(i, 'days')),
+  ];
+}
+
+/**
+ * Mapea una fecha a un objeto para ser usado por Calendar Strip
+ * @param {moment.Moment} moment La fecha a ser convertida
+ * @returns Un objeto del tipo fecha marcada
+ */
+export function momentToMarkedDate(moment, colors) {
   return {
     date: moment,
     dots: [{
-      color: '#07ACB9',
+      color: colors.primary,
       selectedColor: 'white',
     }],
   };
